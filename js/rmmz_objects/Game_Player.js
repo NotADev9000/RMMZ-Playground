d21004ad018f0bab808f394358fc1c2fac3370ff
NotADev9000 <64313023+NotADev9000@users.noreply.github.com>
2023-04-08 19:13:07 +0100

    Misc: Cleanup

--- js/rmmz_objects/Game_Player.js
+++ js/rmmz_objects/Game_Player.js
@@ -325,8 +325,7 @@ Game_Player.prototype.updateDashing = function() {
         return;
     }
     if (this.canMove() && !this.isInVehicle() && !$gameMap.isDashDisabled()) {
-        this._dashing =
-            this.isDashButtonPressed() || $gameTemp.isDestinationValid();
+        this._dashing = this.isDashButtonPressed() || $gameTemp.isDestinationValid();
     } else {
         this._dashing = false;
     }
